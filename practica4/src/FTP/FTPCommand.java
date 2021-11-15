979816f3fc869797326f35e6b3c90ea68a513981
Maqui-LP <maqui-lp@outlook.com>
2021-11-15 20:09:55 -0300

    ftp andando o casi

--- practica4/src/FTP/FTPCommand.java
+++ practica4/src/FTP/FTPCommand.java
@@ -3,26 +3,36 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.io.IOException;
 
 public class FTPCommand {
-    public static byte[] read(String path, int position, int currentSize, long fileSize){
-        int chunck = 1024;
-        int noBytes = ((int)fileSize - currentSize) < chunck
-                ?(int)(fileSize - currentSize)
-                : chunck;
-        byte[] content = new byte[noBytes];
-        try{
-            System.out.println("Reading " + noBytes + " bytes from " + currentSize);
-            InputStream in = new FileInputStream(path);
-            in.skip(currentSize);
-            in.read(content,0,noBytes);
-            in.close();
-        }catch(Exception e){
-            e.printStackTrace();
-            return new byte[0];
+    public static byte[] read(String path, int position, int currentSize, long fileSize)
+        {
+            try {
+                int chunck = 1024;
+                int noBytes = ((int) fileSize - currentSize) < chunck
+                        ? (int) (fileSize - currentSize)
+                        : chunck; 
+                
+                byte[] contents = new byte[noBytes];
+
+                System.out.printf("Reading %d bytes from %d\n", noBytes, currentSize);
+
+                InputStream in = new FileInputStream(path);
+                in.skip(currentSize);
+                in.read(contents, 0, noBytes);
+                in.close();
+
+                return contents;
+            } catch(IOException e) {
+                System.out.println(e);
+                return new byte[0];
+            }
+
+
+
+            
         }
-        return content;
-    }
 
     public static int write(String path, byte[] data, int currentSize, long fileSize){
         try{
